efe076d373047d0c5a13c5a5f8213e8151d72cfc
Courtney Jooste <courtney@codelab.io>
2017-06-12 14:36:31 +0200

    Completed message form (without captcha for now)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,3 +44,43 @@ $('a[href*="#"]')
       }
     }
   });
+
+// $('.send-message').click(function(){
+//   $('#myModal').modal('show');
+// });
+
+$('#errorModal').modal({ show: false});
+$('#successModal').modal({ show: false});
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
